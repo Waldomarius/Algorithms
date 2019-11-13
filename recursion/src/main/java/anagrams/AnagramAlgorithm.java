2137be8c506391a175e96a30c8c338a5be23d748
Waldomarius <kniaziuk.vitaly@yandex.ru>
2019-11-14 00:12:47 +0300

    recursion : anagrams

---
+++ recursion/src/main/java/anagrams/AnagramAlgorithm.java
@@ -0,0 +1,45 @@
+package anagrams;
+
+public class AnagramAlgorithm {
+    private String word;
+    private int size;
+    private char[] arrChar;
+    private int count = 1;
+
+    AnagramAlgorithm(String word) {
+        this.word = word;
+        size = word.length();
+        arrChar =  word.toCharArray();
+    }
+
+    public void runAnagram(){
+        doAnagram(size);
+    }
+
+    private void doAnagram(int newLength) {
+        if (newLength == 1) return;
+        for (int i = 0; i < newLength; i++) {
+            doAnagram(newLength - 1);
+            if (newLength == 2) displayWord();
+            rotateWord(newLength);
+        }
+    }
+
+    private void displayWord() {
+        System.out.print(count++ + " : ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arrChar[i]);
+        }
+        System.out.println();
+    }
+
+    private void rotateWord(int newLength) {
+        int position = size - newLength;
+        char temp = arrChar[position];
+        int i;
+        for (i = position + 1; i < size; i++) {
+            arrChar[i - 1] = arrChar[i];
+        }
+        arrChar[i - 1] = temp;
+    }
+}
---
+++ recursion/src/main/java/anagrams/AnagramApp.java
@@ -0,0 +1,24 @@
+package anagrams;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AnagramApp {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.print("Enter a word:");
+        String word = getWord();
+
+        AnagramAlgorithm anagramAlgorithm = new AnagramAlgorithm(word);
+        anagramAlgorithm.runAnagram();
+    }
+
+    private static String getWord() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        return br.readLine();
+    }
+}
