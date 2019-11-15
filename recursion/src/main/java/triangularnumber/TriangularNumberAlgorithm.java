fd635c83bd1610a91c09b4af99ec73cfc33308e3
Waldomarius <kniaziuk.vitaly@yandex.ru>
2019-11-15 19:50:36 +0300

    recursion : triangular number

---
+++ recursion/src/main/java/triangularnumber/TriangularNumberAlgorithm.java
@@ -0,0 +1,8 @@
+package triangularnumber;
+
+public class TriangularNumberAlgorithm {
+    public long triangularNumber(long number) {
+        if (number == 1) return 1;
+        return number + triangularNumber(number - 1);
+    }
+}
---
+++ recursion/src/main/java/triangularnumber/TriangularNumberApp.java
@@ -0,0 +1,23 @@
+package triangularnumber;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TriangularNumberApp {
+
+    private static long number;
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Enter a number:");
+        TriangularNumberAlgorithm factorialAlgorithm = new TriangularNumberAlgorithm();
+        number = Long.parseLong(getNumber());
+        System.out.print("Result:" + factorialAlgorithm.triangularNumber(number));
+    }
+
+    private static String getNumber() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        return br.readLine();
+    }
+}
